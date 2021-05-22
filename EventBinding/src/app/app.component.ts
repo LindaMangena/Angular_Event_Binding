45fd532b80a767d6b54e21060cd72f22f41c63bf
LindaMangena <LindaJazz42@gmail.com>
2021-05-22 15:49:41 -0400

    added ClickEvent

--- EventBinding/src/app/app.component.ts
+++ EventBinding/src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'MyFirstTodoApp';
+
+
+ngOnInit(){}
+
+  onClick(){
+    console.log('On Click event working');
+  }
 }
